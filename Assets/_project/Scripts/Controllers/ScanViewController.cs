402a8ea93dd5a1470ddd2b6c49bb67b868e4aba7
Stellart420 <93067078+Stellart420@users.noreply.github.com>
2023-06-20 17:33:00 +0700

    +

--- Assets/_project/Scripts/Controllers/ScanViewController.cs
+++ Assets/_project/Scripts/Controllers/ScanViewController.cs
@@ -1,131 +1,141 @@
-//using UnityEngine;
-//using UnityEngine.XR.ARFoundation;
-//using UnityEngine.XR.ARSubsystems;
-//using UnityEngine.UI;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System;
+using System.Diagnostics.Tracing;
 
-//public class ScanViewController : MonoBehaviour
-//{
-//    public enum ScanMode
-//    {
-//        Noneed,
-//        Doing,
-//        Done
-//    }
+public class ScanViewController : MonoBehaviour
+{
+    public enum ScanMode
+    {
+        Noneed,
+        Doing,
+        Done
+    }
 
-//    public ARSession arSession;
-//    public ARMeshManager arMeshManager;
-//    public ARCameraManager arCameraManager;
-//    public ARPlaneManager arPlaneManager;
-//    public ARPointCloudManager arPointCloudManager;
-//    public ARSessionOrigin arSessionOrigin;
-//    public ARRaycastManager arRaycastManager;
-//    public GameObject scanButton;
-//    public GameObject resetButton;
+    public ARSession arSession;
+    public ARMeshManager arMeshManager;
+    public ARCameraManager arCameraManager;
+    public ARPlaneManager arPlaneManager;
+    public ARPointCloudManager arPointCloudManager;
+    public ARSessionOrigin arSessionOrigin;
+    public ARRaycastManager arRaycastManager;
+    public GameObject scanButton;
+    public GameObject resetButton;
 
-//    public LabelScene labelScene;
-//    public ScanMode scanMode = ScanMode.Noneed;
-//    public Texture2D originalSource;
+    public LabelScene labelScene;
+    public ScanMode scanMode = ScanMode.Noneed;
+    public Texture2D originalSource;
 
-//    private void Start()
-//    {
-//        arMeshManager.meshPrefab.SetActive(false);
-//        labelScene.SetText("Scan");
-//    }
+    private void Start()
+    {
+        arMeshManager.meshesChanged += MeshesChanged;
+        arMeshManager.enabled = false;
+        labelScene.SetText("Scan");
+    }
 
-//    public void RotateMode()
-//    {
-//        switch (scanMode)
-//        {
-//            case ScanMode.Noneed:
-//                scanMode = ScanMode.Doing;
-//                labelScene.SetText("Reset");
-//                originalSource = arCameraManager.cameraMaterial.GetTexture("_MainTex") as Texture2D;
-//                arCameraManager.cameraMaterial.SetTexture("_MainTex", null);
-//                arCameraManager.startingMode = ARCameraManager.CameraMode.PlainTexture;
-//                arCameraManager.requestedCameraMode = ARCameraManager.CameraMode.PlainTexture;
-//                arCameraManager.enabled = true;
-//                arCameraManager.StartCamera();
-//                break;
-//            case ScanMode.Doing:
-//                break;
-//            case ScanMode.Done:
-//                scanAllGeometry(false);
-//                scanMode = ScanMode.Noneed;
-//                labelScene.SetText("Scan");
-//                arCameraManager.cameraMaterial.SetTexture("_MainTex", originalSource);
-//                arCameraManager.startingMode = ARCameraManager.CameraMode.Environment;
-//                arCameraManager.requestedCameraMode = ARCameraManager.CameraMode.Environment;
-//                arCameraManager.enabled = true;
-//                arCameraManager.StartCamera();
-//                break;
-//        }
-//    }
+    private void MeshesChanged(ARMeshesChangedEventArgs eventArgs)
+    {
+        foreach (var meshFilter in eventArgs.added)
+        {
+            OnMeshGenerated(meshFilter);
+        }
 
-//    public void SetLabelPosition(Vector2 position)
-//    {
-//        labelScene.transform.position = position;
-//    }
+        foreach (var meshFilter in eventArgs.updated)
+        {
+            OnMeshUpdated(meshFilter);
+        }
 
-//    private void Update()
-//    {
-//        if (scanMode == ScanMode.Doing)
-//        {
-//            scanAllGeometry(true);
-//            scanMode = ScanMode.Done;
-//        }
-//    }
+        foreach (var meshFilter in eventArgs.removed)
+        {
+            OnMeshRemoved(meshFilter);
+        }
+    }
 
-//    public void OnMeshGenerated(ARMeshUpdatedEventArgs eventArgs)
-//    {
-//        ARMeshFilter meshFilter = eventArgs.mesh.GetComponent<ARMeshFilter>();
-//        meshFilter.mesh = eventArgs.mesh;
-//    }
+    public void RotateMode()
+    {
+        switch (scanMode)
+        {
+            case ScanMode.Noneed:
+                scanMode = ScanMode.Doing;
+                labelScene.SetText("Reset");
+                originalSource = arCameraManager.cameraMaterial.GetTexture("_MainTex") as Texture2D;
+                arCameraManager.cameraMaterial.SetTexture("_MainTex", null); 
+                arCameraManager.subsystem.requestedCamera = Feature.AnyCamera;
+                arCameraManager.enabled = true;
+                arCameraManager.subsystem.Start();
+                break;
+            case ScanMode.Doing:
+                break;
+            case ScanMode.Done:
+                scanAllGeometry(false);
+                scanMode = ScanMode.Noneed;
+                labelScene.SetText("Scan");
+                arCameraManager.cameraMaterial.SetTexture("_MainTex", originalSource);
+                arCameraManager.enabled = true;
+                arCameraManager.subsystem.Start();
+                break;
+        }
+    }
 
-//    public void OnMeshRemoved(ARMeshRemovedEventArgs eventArgs)
-//    {
-//        Destroy(eventArgs.mesh);
-//    }
+    public void SetLabelPosition(Vector2 position)
+    {
+        labelScene.transform.position = position;
+    }
 
-//    public void OnMeshUpdated(ARMeshUpdatedEventArgs eventArgs)
-//    {
-//        ARMeshFilter meshFilter = eventArgs.mesh.GetComponent<ARMeshFilter>();
-//        meshFilter.mesh = eventArgs.mesh;
-//    }
+    private void Update()
+    {
+        if (scanMode == ScanMode.Doing)
+        {
+            scanAllGeometry(true);
+            scanMode = ScanMode.Done;
+        }
+    }
 
-//    public void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
-//    {
-//        XRCameraImage image;
-//        if (arCameraManager.TryGetLatestImage(out image))
-//        {
-//            int width = image.width;
-//            int height = image.height;
-//            Texture2D cameraTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
-//            cameraTexture.LoadRawTextureData(image.GetPlane(0).data);
-//            cameraTexture.Apply();
+    public void OnMeshGenerated(MeshFilter meshFilter)
+    {
+    }
 
-//            if (scanMode == ScanMode.Doing)
-//            {
-//                arCameraManager.startingMode = ARCameraManager.CameraMode.Environment;
-//                arCameraManager.requestedCameraMode = ARCameraManager.CameraMode.Environment;
-//                arCameraManager.enabled = true;
-//                arCameraManager.StartCamera();
-//                scanAllGeometry(false);
-//                scanMode = ScanMode.Noneed;
-//                labelScene.SetText("Scan");
-//                arCameraManager.cameraMaterial.SetTexture("_MainTex", originalSource);
-//            }
+    public void OnMeshRemoved(MeshFilter meshFilter)
+    {
+        Destroy(meshFilter.gameObject);
+    }
 
-//            image.Dispose();
-//        }
-//    }
+    public void OnMeshUpdated(MeshFilter meshFilter)
+    {
+    }
 
-//    private void scanAllGeometry(bool needTexture)
-//    {
-//        foreach (ARMeshFilter meshFilter in FindObjectsOfType<ARMeshFilter>())
-//        {
-//            meshFilter.gameObject.SetActive(needTexture);
-//        }
-//    }
-//}
+    public void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
+    {
+        if (arCameraManager.TryAcquireLatestCpuImage(out var image))
+        {
+            int width = image.width;
+            int height = image.height;
+            Texture2D cameraTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            cameraTexture.LoadRawTextureData(image.GetPlane(0).data);
+            cameraTexture.Apply();
+
+            if (scanMode == ScanMode.Doing)
+            {
+                arCameraManager.enabled = true;
+                arCameraManager.subsystem.Start();
+                scanAllGeometry(false);
+                scanMode = ScanMode.Noneed;
+                labelScene.SetText("Scan");
+                arCameraManager.cameraMaterial.SetTexture("_MainTex", originalSource);
+            }
+
+            image.Dispose();
+        }
+    }
+
+    private void scanAllGeometry(bool needTexture)
+    {
+        foreach (MeshFilter meshFilter in FindObjectsOfType<MeshFilter>())
+        {
+            meshFilter.gameObject.SetActive(needTexture);
+        }
+    }
+}
