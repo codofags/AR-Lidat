1430dcc1595f99ecb93a7509bcfe37d6445c7769
Stellart420 <93067078+Stellart420@users.noreply.github.com>
2023-06-23 02:09:58 +0700

    asd

--- Assets/_project/Scripts/Utils/Texture2DExtensions.cs
+++ Assets/_project/Scripts/Utils/Texture2DExtensions.cs
@@ -3,21 +3,6 @@ using UnityEngine;
 
 public static class Texture2DExtensions
 {
-    public static Texture2D CropTexture(this Texture2D texture, Rect rect)
-    {
-        int x = Mathf.FloorToInt(rect.x);
-        int y = Mathf.FloorToInt(rect.y);
-        int width = Mathf.FloorToInt(rect.width);
-        int height = Mathf.FloorToInt(rect.height);
-
-        Color[] pixels = texture.GetPixels(x, y, width, height);
-        Texture2D croppedTexture = new Texture2D(width, height);
-        croppedTexture.SetPixels(pixels);
-        croppedTexture.Apply();
-
-        return croppedTexture;
-    }
-
     public static void RotateTexture(this Texture2D texture, bool clockwise)
     {
         Color32[] original = texture.GetPixels32();
