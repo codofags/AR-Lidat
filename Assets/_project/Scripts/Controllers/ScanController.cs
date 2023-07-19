8e41b13ffb763f4eb7f402373e4f9b6fe69c48af
Stellart420 <93067078+Stellart420@users.noreply.github.com>
2023-07-19 18:13:51 +0700

    fix

--- Assets/_project/Scripts/Controllers/ScanController.cs
+++ Assets/_project/Scripts/Controllers/ScanController.cs
@@ -2,6 +2,7 @@ using HoloGroup.Networking.Internal.Sockets;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
