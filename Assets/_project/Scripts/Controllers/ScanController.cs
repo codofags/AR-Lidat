ae45c03c197232ae994f9e88170899109f739d9e
Stellart420 <93067078+Stellart420@users.noreply.github.com>
2023-07-19 15:42:25 +0700

    save frame disabled

--- Assets/_project/Scripts/Controllers/ScanController.cs
+++ Assets/_project/Scripts/Controllers/ScanController.cs
@@ -88,7 +88,7 @@ public class ScanController : Singleton<ScanController>
                 arMeshSubsystem.Start();
                 _isScanning = true;
                 //StartCoroutine(Scaning());
-                CameraPositionSaver.Instance.StartSaving();
+                //CameraPositionSaver.Instance.StartSaving();
                 Debug.Log("Scan START");
             }
             _isScanning = true;
